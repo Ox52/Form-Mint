160e6c605c92a52c054063bab737a27cb41b36ca
Ox52 <harsh04241@gmail.com>
2025-12-04 16:15:55 +0530

    added form

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,60 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Dashboard() {
 
   return (
-    <div className='space-y-6' >
+    <div className="space-y-6">
+      <div>
+        <h1 className="text-3xl font-bold "> Welcome, Harsh</h1>
+        <p className=" text-gray-500 mt-1"> manage your forms and responses</p>
+      </div>
 
-        <div>
+      <div className=" grid grid-cols-1 gap-4 md:grid-cols-3">
+        <div className="bg-white rounded-lg shadow p-6 border">
+          <h2 className="text-xl font-medium "> Your Forms</h2>
+          <p className="text-3xl font-bold mt-2 ">12</p>
 
-            <h1 className='text-3xl font-bold '>  Welcome, Harsh</h1>
-            <p className=' text-gray-500 mt-1'> manage your forms and responses</p>
+          <Button className="mt-4 " asChild>
+            <Link href="/dashboard/forms"> View All Forms</Link>
+          </Button>
         </div>
-      
+        <div className="bg-white rounded-lg shadow p-6 border">
+          <h2 className="text-xl font-medium "> Total Responses</h2>
+          <p className="text-3xl font-bold mt-2 ">100</p>
+        </div>
+        <div className="bg-white rounded-lg shadow p-6 border">
+          <h2 className="text-xl font-medium "> Create New</h2>
+          <p className="text-gray-500 mt-2 ">Start building a new form </p>
+
+          <Button className="mt-4 " asChild>
+            <Link href="/dashboard/forms"> Create Form </Link>
+          </Button>
+        </div>
+      </div>
+
+      <div className="bg-white rounded-lg shadow p-6 border">
+        <h2 className="text-xl font-medium mb-4"> Recent Forms</h2>
+        <div className="space-y-4">
+          <div className="flex items-center justify-between border-b pb-4">
+            <div>
+              {" "}
+              <h3 className="font-medium"> This is the title</h3>
+              <p> responses. Created on 21 April 2025 </p>
+            </div>
+            <div className="flex gap-2">
+              <Button>
+                <Link href={`/dashboard/forms/123`}> View</Link>
+              </Button>
+
+              <Button>
+                <Link href={`/dashboard/forms/123/response`}> Responses</Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ components/forms/form-builder.tsx
@@ -0,0 +1,119 @@
+'use client'
+import React, { FormEvent, useState } from 'react'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+import { Textarea } from '../ui/textarea'
+import { Button } from '../ui/button'
+import { v4 as uuidv4 } from 'uuid'
+import { toast } from 'sonner'
+
+export default function FormBuilder() {
+
+    const [form,setForm] = useState({
+
+        title:"",
+        description:"",
+        questions:[{
+
+            id:"1",
+            text:"",
+        },]
+    })
+
+    const addQuestion =()=>{
+
+      setForm((prev)=>({
+
+        ...prev, questions:[...prev.questions,{id:uuidv4(),text:""}]
+      }))
+
+    }
+
+    const removeQuestion = (index:number)=>{
+
+      if(form.questions.length >1){
+        setForm(prev => ({
+          ...prev, questions:prev.questions.filter((_,i)=>i!==index)
+        }))
+      }else{
+        toast.error(" Form must have at least one question")
+      }
+
+    }
+    const handelQuestionChange = (index:number , value:string)=>{
+      const updateQuestion= [...form.questions];
+      updateQuestion[index].text=value;
+      setForm({...form,questions:updateQuestion});
+
+    }
+
+    const handelSubmit =( e:FormEvent) =>{
+
+      e.preventDefault();
+
+    }
+  return (
+    <form  onSubmit={handelSubmit} className="space-y-8">
+      <div className="space-y-4">
+        <div>
+          <Label htmlFor="title">Title</Label>
+          <Input
+            id="title"
+            value={form.title}
+            onChange={(e) => setForm({ ...form, title: e.target.value })}
+            placeholder="Enter from title"
+            className="mt-1"
+          />
+        </div>
+        <div>
+          <Label htmlFor="description">Description(Optional)</Label>
+          <Textarea
+            id="title"
+            value={form.description}
+            onChange={(e) => setForm({ ...form, description: e.target.value })}
+            placeholder="Enter from description"
+            className="mt-1"
+          />
+        </div>
+      </div>
+
+      <div className="space-y-6">
+        <div className="flex items-center justify-between">
+          <h3 className="text-lg font-medium">Questions</h3>
+
+          <Button variant="outline" type="button" onClick={addQuestion}>
+            {" "}
+            Add Questoin
+          </Button>
+        </div>
+
+        {form.questions.map((question, index) => (
+          <div key={question.id} className="space-y-2 p-4 border rounded-md ">
+            <div className="flex items-center justify-between">
+              <Label htmlFor={`Question-${index}`}> Question {index + 1}</Label>
+
+              <Button
+                variant="ghost"
+                size="sm"
+                type="button"
+                className="text-red-500 hover:text-red-700"
+                onClick={()=> removeQuestion(index)}
+              >
+                {" "}
+                remove
+              </Button>
+            </div>
+            <Textarea id={`Question-${index}`} value={question.text} onChange={(e)=>{handelQuestionChange(index, e.target.value);}} placeholder='Enter your question ' className='mt-1'/>
+          </div>
+        ))}
+      </div>
+
+      <div className='flex justify-end gao-2'>
+
+        <Button type='button' variant="outline" onClick={()=>{}}> Cancel </Button>
+
+        <Button type='submit'>Save</Button>
+      </div>
+    </form>
+  );
+}
